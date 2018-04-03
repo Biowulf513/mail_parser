d5dc0b83d8fb6ad95f11028dc259eef4c7714efc
Biowulf513 <cherepanov92@gmail.com>
2018-04-03 15:11:44 +0500

    2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в словарь

--- my_solution.py
+++ my_solution.py
@@ -4,7 +4,7 @@ __email__ = "cherepanov92@gmail.com"
 
 '''
 +1. Построчно читаем файл
-+2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в список
++2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в словарь
  3. Как только в список попадает строка со статусом 'removed' передаём список в обработку
      1. Выясняем статус отправки
      2. Обрабатываем список в зависимости от статуса
@@ -12,11 +12,11 @@ __email__ = "cherepanov92@gmail.com"
  4. Подсчтиываем почтовые адреса и статусы
  5. Генерируем и возвращаем CSV
 '''
-import re, time
+import re
 
 class SearchMessageInLog:
     reg_message_id = r'([0-9A-Z]{11})'
-    message_list = []
+    all_messages = {}
     counter = 0
 
     def file_reader(self, file_name):
@@ -28,15 +28,11 @@ class SearchMessageInLog:
 
     def find_message_in_array(self, message_obj):
         id, log_record = message_obj
-        rec = 0
 
-        for message_record in self.message_list:
-            if message_record[0] == id:
-                message_record.append(log_record)
-                rec = 1
-
-        if not rec:
-            self.message_list.append(list([id, log_record]))
+        if id in self.all_messages:
+            self.all_messages[id].append(log_record)
+        else:
+            self.all_messages.update({id:[log_record]})
 
 if __name__ == '__main__':
     i = SearchMessageInLog()
