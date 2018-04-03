381a27e44338f8b6fecd23964e3191782d9a6446
Biowulf513 <cherepanov92@gmail.com>
2018-04-03 12:16:02 +0500

    2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в список

--- my_solution.py
+++ my_solution.py
@@ -4,18 +4,40 @@ __email__ = "cherepanov92@gmail.com"
 
 '''
 +1. Построчно читаем файл
- 2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в словарь
- 3. Передаём словари в обработку
++2. Если в строке есть id сообщения - 11 символов (числа и буквы в верхнем регистре) собираем их в список
+ 3. Как только в список попадает строка со статусом 'removed' передаём список в обработку
      1. Выясняем статус отправки
      2. Обрабатываем список в зависимости от статуса
      3. Сохраняем адрес отправителя и статус отправки
  4. Подсчтиываем почтовые адреса и статусы
  5. Генерируем и возвращаем CSV
 '''
-def file_reader(file_name):
-    with open(file_name, mode='r') as f:
-        for line in f:
-            print(line)
+import re
+import time
+
+class SearchMessageInLog:
+    reg_message_id = r'([0-9A-Z]{11})'
+    message_list = []
+    counter = 0
+
+    def file_reader(self, file_name):
+        with open(file_name, mode='r') as f:
+            for line in f:
+                message_id = re.search(self.reg_message_id, line)
+                if message_id:
+                    self.message_array(line.rstrip(), message_id.group())
+
+    def message_array(self, message, message_id):
+        for message_record in self.message_list:
+            if message_record[0] == message_id:
+                print('Повтор')
+                message_record.append(message)
+                break
+            else:
+                continue
+
+        self.message_list.append(list([message_id, message]))
 
 if __name__ == '__main__':
-    file_reader('maillog')
+    i = SearchMessageInLog()
+    i.file_reader('maillog')
